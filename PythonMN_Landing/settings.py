a5fdf01783b9ecf0f8db63a295d1ae54b65518e7
aaron <aaolson817@gmail.com>
2015-04-18 20:52:07 -0500

    Cleaned up settings.

--- PythonMN_Landing/settings.py
+++ PythonMN_Landing/settings.py
@@ -11,17 +11,12 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import sys
 import os
-import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__) + '/../')
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__) + '/../../')
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'uz!w_&y7h+tn+oba#*eu^o!f!bev=hgppa=%*fz+#+m*fz+b2z'
@@ -79,16 +74,14 @@ ROOT_URLCONF = 'PythonMN_Landing.urls'
 WSGI_APPLICATION = 'PythonMN_Landing.wsgi.application'
 
 # DATABASE
-# DATABASES = {'default': dj_database_url.config()}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'python',  # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'python',
         'USER': 'python',
         'PASSWORD': 'python',
-        'HOST': 'localhost',    # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '',      # Set to empty string for default.
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 CONN_MAX_AGE = 600
@@ -112,7 +105,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR,  'conference/templates'),
 )
 
 if DEBUG:
