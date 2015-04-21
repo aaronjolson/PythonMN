6bffd586c3b238ba213181dce24b03db11073151
aaron <aaolson817@gmail.com>
2015-04-21 18:25:22 -0500

    - simplified static file routing in settings

--- PythonMN_Landing/settings.py
+++ PythonMN_Landing/settings.py
@@ -104,18 +104,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'conference/templates'),
-)
-
-if DEBUG:
-    MEDIA_URL = '/media/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static-only')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'), 'static'
-    )
-
 PIPELINE_CSS = {
     'pythonMN_base': {
         'source_filenames': (
