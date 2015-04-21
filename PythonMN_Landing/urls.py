ce58024948752f484d47e4fab9ef92d16033f52f
aaron <aaolson817@gmail.com>
2015-04-21 18:25:00 -0500

    - simplified static file routing in urls

--- PythonMN_Landing/urls.py
+++ PythonMN_Landing/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
 
 from django.contrib import admin
 
@@ -12,7 +11,6 @@ urlpatterns = patterns('',
                        )
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += patterns(
+        url(r'', include('django.contrib.staticfiles.urls')),
+    )
